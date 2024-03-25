cf350186312197c38c32ea24b28edc9a8fb178d0
Glhazyanno Linhares Braga <glbraga9@gmail.com>
2024-03-25 16:57:20 -0300

    Lista java

---
+++ javascript.js
@@ -0,0 +1,16 @@
+// Elementos 
+
+
+const input = document.querySelector('input');
+const lista = document.querySelector('ul');
+
+// Funções 
+function handleKeyUp(e){
+    if (e.key === 'Enter'){
+        lista.inner
+    }
+}
+
+// Eventos
+
+input.addEventListe
